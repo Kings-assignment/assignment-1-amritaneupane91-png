bd771a03cef08ef7a0237141e5fe3ac0b19fd08c
amritaneupane91-png <amritaneupane91@gmail.com>
2026-02-02 12:03:18 +0545

    assignment9

--- src/q9.c
+++ src/q9.c
@@ -2,3 +2,16 @@
 // Example: 
 // Input = 10
 // Output = "1 week and 3 days"
+
+#include <stdio.h>
+    int main() {
+    int number;
+    int week,remaining_days;
+
+    printf("Enter a number:");
+    scanf("%d",&number);
+    week=number/7;
+    remaining_days=number%7;
+    printf("%d week %d days",week,remaining_days);
+    return 0;
+}
