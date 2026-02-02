d19f70589f71478e4c45da51698d8f2100a9a3ef
amritaneupane91-png <amritaneupane91@gmail.com>
2026-02-02 11:43:26 +0545

    assignment 6

--- src/q6.c
+++ src/q6.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 
 int main(){
-
-
-
-    
+int length,width;
+int area;
+printf("Enter the length and width:");
+scanf("%d %d",&length,&width);
+area=length*width;
+printf("The area of rectangle is %d",area);
+return 0;
 }
