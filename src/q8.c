59b025308257f3492fa614f1a9a33b36e6b84919
amritaneupane91-png <amritaneupane91@gmail.com>
2026-02-02 11:51:16 +0545

    assignment 8

--- src/q8.c
+++ src/q8.c
@@ -1,3 +1,14 @@
 /* Write a C program to convert temperature from Celsius to Fahrenheit. Prompt the user for a
 temperature in Celsius and display the equivalent temperature in Fahrenheit.
 (Formula: fahrenheit = (celsius * 9 / 5) + 32) */
+
+#include <stdio.h>
+
+int main(){
+    float celsius,fahrenheit;
+    printf("Enter the celsius:");
+    scanf("%f",&celsius);
+    fahrenheit=(celsius*9/5)+32;
+    printf("The converted temperature is %f",fahrenheit);
+    return 0;
+}
