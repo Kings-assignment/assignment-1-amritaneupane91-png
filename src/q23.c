0a1f2142fa4d3430fc9ad0a16c9f6abe3c16330c
amritaneupane91-png <amritaneupane91@gmail.com>
2026-02-05 11:07:15 +0545

    number 23

--- src/q23.c
+++ src/q23.c
@@ -1,2 +1,20 @@
 // Given a variable num, write an expression that checks if it is a power of 2.
 // You can you math header file for this (eg: #include <math.h>)
+
+#include <stdio.h>
+
+int main (){
+
+int num;
+int result;
+
+prinmtf("Enter a number:");
+scanf("%d", &num);
+
+result= !(num & (num -1));
+
+printf("The result is %d\n", result);
+
+return 0;
+
+}
